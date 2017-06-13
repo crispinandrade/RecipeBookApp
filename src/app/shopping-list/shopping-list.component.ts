ddb8a6a1064955d956ee7bd5097a1c7a9364b21b
crispinandrade95 <crispin.andrade95@gmail.com>
2017-06-13 19:03:11 +1000

    Barebones components
    Completed the basic structure of each component

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+  ingredients: Ingredient[] = [
+    new Ingredient('Lettuce', 5),
+    new Ingredient('Tomatoes', 5),
+    new Ingredient('Bun', 5),
+    new Ingredient('Cheese', 5),
+    new Ingredient('Beef patty', 5),
+  ];
 
   constructor() { }
 
